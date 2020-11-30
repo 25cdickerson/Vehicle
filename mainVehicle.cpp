128ba69037d280ee94396802f85d40ce85bead53
25cdickerson <73359174+25cdickerson@users.noreply.github.com>
2020-11-29 19:34:25 -0500

    Finished Main, and All other classes
    Finished the main, vehicle, truck, and car classes. This includes the output on the console in the main.

--- mainVehicle.cpp
+++ mainVehicle.cpp
@@ -3,11 +3,71 @@
 // CIS1202
 
 #include <iostream>
+#include <string>
+
+#include "Vehicle.h"
+#include "Car.h"
+#include"Truck.h"
 
 using namespace std;
 
 int main()
 {
+	string Manufacturer;
+	string y;
+	int year;
+
+	cout << "Vehicle Program\n\n";
+	cout << "Vehicle:\n";
+	cout << "Enter Manufacturer: ";
+	getline(cin, Manufacturer);
+	cout << "\n";
+	cout << "Enter Year Built: ";
+	getline(cin, y);
+	year = stoi(y);
+	cout << "\nVehicle Info\n";
+
+	Vehicle v(Manufacturer, year);
+	v.displayInfo();
+	cout << "\n\n";
+
+	int numDoor;
+	cout << "Car:\n";
+	cout << "Enter Manufacturer: ";
+	getline(cin, Manufacturer);
+	cout << "\n";
+	cout << "Enter Year Built: ";
+	getline(cin, y);
+	year = stoi(y);
+	cout << "Enter Number of Doors: ";
+	getline(cin, y);
+	numDoor = stoi(y);
+
+	cout << "\nCar Info\n";
+	Car c(numDoor);
+	c.setMan(Manufacturer);
+	c.setYr(year);
+	c.displayInfo();
+	cout << "\n\n";
+
+	int tow;
+	cout << "Truck:\n";
+	cout << "Enter Manufacturer: ";
+	getline(cin, Manufacturer);
+	cout << "\n";
+	cout << "Enter Year Built: ";
+	getline(cin, y);
+	year = stoi(y);
+	cout << "Tow Capacity: ";
+	getline(cin, y);
+	tow = stoi(y);
+	
+	cout << "\nTruck Info\n";
+	Truck t(tow);
+	t.setMan(Manufacturer);
+	t.setYr(year);
+	t.displayInfo();
+	cout << "\n\n";
 
 }
 
